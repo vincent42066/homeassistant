203181ff7f0eab364eeed9c1d37e741223002080
Vincent <vincent42066@gmail.com>
2019-03-12 16:23:47 +0000

    write sensors or effector to json file with mqtt

--- addsensor.py
+++ addsensor.py
@@ -2,16 +2,36 @@ import time
 import paho.mqtt.client as paho
 import json
 broker="172.20.10.13"
+outfile = "ble_devices.json"
 #define callback
 def on_message(client, userdata, message):
     time.sleep(1)
     m_decode=str(message.payload.decode("utf-8","ignore"))
     dictMsg=json.loads(m_decode)
     dictMsg['topic'] = "polytech/" + dictMsg['nom'].replace(" ", "").lower()
-    print(dictMsg)
-    with open("sensors.yaml", "a") as myfile:
-        myfile.write("\r\n- platform: mqtt\r\n  state_topic: \"" + dictMsg['topic'] + "\"\r\n  name: \"" + dictMsg['nom'] + "\"\r\n  value_template: '{{ value_json.unit }}'")
-        myfile.close
+    print(message.topic)
+    if message.topic == "polytech/ajoutercapteur":
+        dictMsg['type'] = "capteur"
+        print(dictMsg)
+        with open("sensors.yaml", "a") as myfile:
+            myfile.write("\r\n- platform: mqtt\r\n  state_topic: \"" + dictMsg['topic'] + "\"\r\n  name: \"" + dictMsg['nom'] + "\"\r\n  value_template: '{{ value_json.unit }}'")
+            myfile.close
+    elif message.topic == "polytech/ajoutereffecteur":
+        dictMsg['type'] = "effecteur"
+        print(dictMsg)
+        with open("switch.yaml", "a") as myfile:
+            myfile.write("\r\n- platform: mqtt\r\n  command_topic: \"" + dictMsg['topic'] + "\"\r\n  name: \"" + dictMsg['nom']+ "\"\r\n")
+            myfile.close
+    with open(outfile, 'r') as json_file:
+        print('coucou')
+        dictFromFile = json.load(json_file)
+        print(dictFromFile)
+        for key in dictMsg.keys():
+            dictFromFile[key].append(dictMsg[key])
+        print(dictFromFile)
+    with open(outfile, 'w') as json_file:
+            json.dump(dictFromFile, json_file, indent=2)
+
 
 client= paho.Client("client-001")
 ######Bind function to callback
@@ -22,6 +42,7 @@ client.connect(broker)#connect
 client.loop_start() #start loop to process received messages
 print("subscribing ")
 client.subscribe("polytech/ajoutercapteur")#subscribe
+client.subscribe("polytech/ajoutereffecteur")#subscribe
 time.sleep(2)
 # client.disconnect() #disconnect
 # client.loop_stop() #stop loop
