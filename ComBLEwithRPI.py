a13958e1978cce0ea7081ec4f5bf8492ba8f2cf1
Vincent <vincent42066@gmail.com>
2019-03-13 17:32:00 +0000

    com between rpi HA and hm10 working

--- ComBLEwithRPI.py
+++ ComBLEwithRPI.py
@@ -8,64 +8,42 @@ import json
 import paho.mqtt.client as paho
 
 # Tool for debug PyGatt
-import logging
-logging.basicConfig()
-logging.getLogger('pygatt').setLevel(logging.DEBUG)
+# import logging
+# logging.basicConfig()
+# logging.getLogger('pygatt').setLevel(logging.DEBUG)
 
-bleDevices = {
-  "mac": [
-    "7C:01:0A:7C:2E:76",
-    "7C:01:0A:7C:2E:76",
-    "7C:01:0A:7C:2E:76",
-  ],
-  "nom": [
-    "vincentcapteur",
-    "vincenteffecteur",
-    "MQTT Effecteur",
-  ],
-  "type": [
-    "capteur",
-    "effecteur",
-    "effecteur",
-  ],
-  "topic": [
-    "polytech/vincentcapteur",
-    "polytech/vincenteffecteur",
-    "home/lovelace/volet/set",
-  ]
-}
-
+outfile = "/home/homeassistant/.homeassistant/ble_devices.json"
 addrMACBLElist = ['7C:01:0A:7C:2E:76']
 adapter = pygatt.GATTToolBackend()
 uuidBLE = "0000ffe1-0000-1000-8000-00805f9b34fb"
 broker="172.20.10.13"
 
-def write(data, write_uuid, device):
-    device.char_write(write_uuid, unhexlify(data))
+with open(outfile, 'r') as json_file:
+    bleDevices = json.load(json_file)
 
 def on_message(client, userdata, message):
+    """
+    Cette fonction a pour but de recupérer les packet mqtt venant de home
+    assistant pour les effecteur et d'envoyer la commande au module BLE.
+    """
     m_decode=str(message.payload.decode("utf-8","ignore"))
-    print(m_decode)
-    addr = bleDevices['mac'][bleDevices['topic'].index(message.topic)]
-    print(addr)
-    try:
-        adapter.start()
-        device = adapter.connect(addr)  # Connection to BLE
-        print('m_decode')
-        print(m_decode)
-        if m_decode == "ON":
-            print('1')
-            device.char_write(uuidBLE, str.encode("1"))
-        elif m_decode == "OFF":
-            print('0')
-            device.char_write(uuidBLE, str.encode("0"))
-    except Exception as e:
-        print('caca')
-        logging.error(traceback.format_exc())
-
-        adapter.stop()
-    finally:
-        adapter.stop()
+    if bleDevices['type'][bleDevices['topic'].index(message.topic)] == "effecteur":
+        # print(m_decode)
+        addr = bleDevices['mac'][bleDevices['topic'].index(message.topic)]
+        # print(addr)
+        try:
+            adapter.start()
+            device = adapter.connect(addr)  # Connection to BLE
+            if m_decode == "ON":
+                # print('1')
+                device.char_write(uuidBLE, str.encode("1"))
+            elif m_decode == "OFF":
+                # print('0')
+                device.char_write(uuidBLE, str.encode("0"))
+        except Exception as e:
+            print('ca marche pas')
+        finally:
+            adapter.stop()
 
 client= paho.Client("client-002")
 ######Bind function to callback
@@ -79,30 +57,33 @@ for topic in bleDevices['topic']:
     print(topic)
     client.subscribe(topic)#subscribe
 
+
+def handle_data(handle, value):
+    """
+    handle -- integer, characteristic read handle the data was received on
+    value -- bytearray, the data returned in the notification
+    """
+    # print("Received data: %s" % hexlify(value))
+    mqttJson = {"unit": value.decode("utf-8","ignore")}
+    print(mqttJson)
+    data_out=json.dumps(mqttJson)
+    client.publish(actualTopic, data_out)
+
 while True:
-    time.sleep(5)  # Délai avant de réveiller les module HM-10
-
-
-#
-# def handle_data(handle, value):
-#     """
-#     handle -- integer, characteristic read handle the data was received on
-#     value -- bytearray, the data returned in the notification
-#     """
-#     print("Received data: %s" % hexlify(value))
-#
-# while True:
-#     time.sleep(5)  # Délai avant de réveiller les module HM-10
-#     for addr in addrMACBLElist:  # Baylaye des module HM-10
-#         try:
-#             adapter.start()
-#             device = adapter.connect(addr)  # Connection to BLE
-# #            write(hexlify(str.encode("WAKEUP")), uuidBLE) # Réveillez le HM-10
-#             device.subscribe(uuidBLE, # Read data from BLE at any time
-#                      callback=handle_data)
-#             time.sleep(3)
-#         except:
-#             print("ça marche pas")
-#             adapter.stop()
-#         finally:
-#             adapter.stop()
+    time.sleep(20)  # Délai avant de réveiller les modules HM-10
+    with open(outfile, 'r') as json_file: # lecture du fichier json
+        bleDevices = json.load(json_file)
+        # print(bleDevices)
+    for index in range(len(bleDevices['mac'])):  # Baylaye des modules HM-10
+        if bleDevices['type'][index] == "capteur":
+            actualTopic = bleDevices['topic'][index]
+            try:
+                adapter.start()
+                device = adapter.connect(bleDevices['mac'][index])  # Connection to BLE
+                device.subscribe(uuidBLE, # Read data from BLE at any time
+                         callback=handle_data)
+                time.sleep(3)
+            except Exception as e:
+                print('ça marche pas ')
+            finally:
+                adapter.stop()
